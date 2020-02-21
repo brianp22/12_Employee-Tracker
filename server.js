1760111fb6ff3e6297757f69d03a354c5306a12c
Brian Prickril <bprickril22@gmail.com>
2020-02-21 07:47:18 -0500

    added the start function and created, main menu inquirer, and a switch containing a function to be created for each selection

--- server.js
+++ server.js
@@ -12,6 +12,58 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    runSearch();
+    startCMS();
 });
 
+function startCMS() {
+    inquirer
+        .prompt({
+            name: "selection",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "Add Department",
+                "Add Role",
+                "Add Employee",
+                "View all Departments",
+                "View all Roles",
+                "View all Employees",
+                "Update a Role",
+                "exit"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.selection) {
+                case "Add Department":
+                    addDepartment();
+                    break;
+
+                case "Add Role":
+                    addRole();
+                    break;
+
+                case "Add Employee":
+                    addEmployee();
+                    break;
+
+                case "View all Departments":
+                    viewDepartments();
+                    break;
+
+                case "View all Roles":
+                    viewRoles();
+                    break;
+
+                case "View all Employees":
+                    viewEmployees();
+                    break;
+
+                case "Update a Role":
+                    updateRole();
+                    break;
+                case "exit":
+                    connection.end();
+                    break;
+            }
+        });
+}
